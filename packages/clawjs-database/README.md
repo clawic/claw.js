61260bbb6059616b07b9eb61e6f52abc50545d40
Iván González Dávila <ivangdavila@creatinium.com>
2026-04-21 19:01:04 +0200

    feat(database): add shared local database package

---
+++ packages/clawjs-database/README.md
@@ -0,0 +1,3 @@
+# @clawjs/database
+
+Shared database engine, API client, and embeddable service app for ClawJS.
---
+++ packages/clawjs-database/src/index.ts
@@ -0,0 +1,7 @@
+export * from "./app.ts";
+export * from "./auth.ts";
+export * from "./client.ts";
+export * from "./config.ts";
+export * from "./realtime.ts";
+export * from "./store.ts";
+export * from "./types.ts";
