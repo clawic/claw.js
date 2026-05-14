1304b3f243b5c5244af2e4a133df0d794471a34a
Iván González Dávila <ivangdavila@creatinium.com>
2026-05-14 13:44:11 +0200

    refactor(sdk): narrow claw option exports

--- packages/clawjs-node/src/index.ts
+++ packages/clawjs-node/src/index.ts
@@ -26,7 +26,7 @@ export * from "./soul/store.ts";
 export * from "./user/store.ts";
 export * from "./create-claw.ts";
 export type * from "./create-claw-instance.ts";
-export type * from "./create-claw-options.ts";
+export type { ClawFactory, CreateClawOptions } from "./create-claw-options.ts";
 export * from "./data/index.ts";
 export * from "./files/managed-blocks.ts";
 export * from "./files/template-pack.ts";
